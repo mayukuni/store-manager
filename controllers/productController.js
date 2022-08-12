0dd145655e6f89b69373a436621614f8d54e0600
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2022-08-12 03:26:47 -0300

    teste

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,25 +1,24 @@
-const rescue = require('express-rescue');
 const productService = require('../services/productService');
 
 const productController = {
-  getAllProducts: rescue(async (_req, res) => {
+  getAllProducts: async (_req, res) => {
     const products = await productService.getAllProducts();
     res.status(200).json(products);
-  }),
+  },
   
-  getProductById: rescue(async (req, res) => {
+  getProductById: async (req, res) => {
     const { id } = req.params;
     const product = await productService.getProductById(id);
     if (!product) return res.status(404).json({ message: 'Product not found' });
     res.status(200).json(product);
-  }),
+  },
 
-  create: rescue(async (req, res) => {
+  create: async (req, res) => {
     const { name } = req.body;
     const product = await productService.create(name);
     if (!name) return res.status(400).json({ message: 'Name is required' });
     res.status(201).json(product);
-  }),
+  },
 };
 
 // por que o 3 não passa?
