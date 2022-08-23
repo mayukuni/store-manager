f940fd6bcdab3c1e1e6aada20056740c00b9205c
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2022-08-23 00:30:52 -0300

    teste

--- tests/unit/controllers/saleController.test.js
+++ tests/unit/controllers/saleController.test.js
@@ -62,22 +62,5 @@ describe('SaleController', () => {
       expect(res.status.calledWith(200)).to.be.true;
       expect(res.json.calledWith(mock)).to.be.true;
     });
-
-    it('deve retornar status 404 quando o id não existe', async () => {
-      sinon.stub(saleService, 'getById').resolves(length);
-
-      const req = {};
-      const res = {};
-
-      res.status = sinon.stub().returns(res);
-      res.json = sinon.stub();
-
-      req.params = { id: 1001 };
-
-      await saleController.getById(req, res);
-
-      expect(res.status.calledWith(404)).to.be.true;
-      expect(res.json.calledWith({ message: 'Product not found' })).to.be.true;
-    });
   });
 });
